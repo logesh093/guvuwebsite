4b76509d50ae8143cdf736359b16853b9454fbbe
logesh093 <113925010+logesh093@users.noreply.github.com>
2022-11-08 10:59:04 +0530

    Add files via upload

---
+++ test1/login.js
@@ -0,0 +1,14 @@
+function valitate(){
+    var username=document.getElementById("username").nodeValue;
+    var email=document.getElementById("email").nodeValue;
+    var password=document.getElementById("password").nodeValue;
+    var password2=document.getElementById("password2").nodeValue;
+    if(username=="logesh"&&password=="1234")
+    {
+        window.print(username);
+        window.println(email);
+        
+    }
+}
+    
+   
